f24fe7e17c499122a65d235e988a5f4725e3f41d
Wasiu Ramoni <wasiu.a.ramoni@gmail.com>
2023-03-17 00:12:07 -0400

    working on event deligaation

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 /*"https://www.matrix.com/blog/8-things-you-need-to-know-before-going-platinum" // new board to be used
 const heroDescription*/
 
-;(function(){  //protecting myself from surounding codes
+;(function(){  
 
 
 
 
 
    const alertEl = document.querySelector('.alert-owner') 
+   const heroDesc = document.querySelector('.description');
    const close = document.querySelector('.alert-owner span') 
    const btnOwner = document.querySelector('.btn.btn-owner');
-   const heroDesc = document.querySelector('.description');
-   
+   const productContainer = document.querySelector('.product-wrapper');
+
+
    heroDesc.textContent = heroDesc.textContent.replace(/chair/gi, 'salon');
-   
 
-   alertEl.classList.add('hidden') // hidden it 
-
+   alertEl.classList.add('hidden') // hide it at first
    btnOwner.addEventListener('click', function(event){
       alertEl.classList.remove('hidden')
    })
@@ -25,7 +25,10 @@ const heroDescription*/
       alertEl.classList.add('hidden')
    })
  
-
+ //using event delgation to capture targeted product 
+productContainer.addEventListener('click', function(event){
+  console.log(event.target.dataset-prodcut)
+})
 
 
 
